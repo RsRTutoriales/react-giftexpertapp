dcc566a7f50dcadbed1762a22bf470392663630d
br distribucion <brdistribucionIT@outlook.es>
2020-07-21 08:30:26 +0200

    docs agregado

---
+++ docs/precache-manifest.c2e7672d47e4dd8152981ba40c932948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7a4764cc7a22f0ab8d7958443f33cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40929f8e6ffeeea2d19",
+    "url": "/static/css/main.d525671b.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40929f8e6ffeeea2d19",
+    "url": "/static/js/main.1dff411b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
